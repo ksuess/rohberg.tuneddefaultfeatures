4d5d8f9b6e95793cb7c0aecb89f2a95fbda8108a
Katja Süss <k.suess@rohberg.ch>
2024-05-24 14:46:40 +0200

    Fix redirect of contact-info

--- src/rohberg/tuneddefaultfeatures/browser/contact_info.py
+++ src/rohberg/tuneddefaultfeatures/browser/contact_info.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from plone import api
+from Products.CMFPlone import PloneMessageFactory as _pmf
 from Products.CMFPlone.browser.contact_info import ContactForm
+from zope.i18n import translate
+from zope.globalrequest import getRequest
 
 
 class ContactFormCustom(ContactForm):
@@ -13,5 +16,13 @@ class ContactFormCustom(ContactForm):
         """Redirect if the flag is set in control panel."""
         exclude_default_contact_form = api.portal.get_registry_record("tdf.exclude_default_contact_form", default=False)
         if exclude_default_contact_form:
-            return self.response.redirect(self.context.portal_url() + '/kontakt')
+            # https://6.docs.plone.org/i18n-l10n/translating-text-strings.html#manually-translated-message-ids
+            # Translate some text
+            msgid = _pmf("Contact")  # my_text is zope.
+
+            # Use inherited translate() function to get the final text string
+            translated = translate(msgid, context=getRequest())
+            translated_lowercase = translated.lower()
+
+            return self.request.response.redirect(self.context.portal_url() + '/' + translated_lowercase)
         return super(ContactFormCustom, self).__call__()
